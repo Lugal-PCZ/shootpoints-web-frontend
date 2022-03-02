230cac34acaae668e38cbe7371b73a03e9c93509
Paul C. Zimmerman <paul@lugal.com>
2022-03-02 10:10:11 -0500

    Regroup and alphabetize methods in file.

--- scripts.js
+++ scripts.js
@@ -1,51 +1,4 @@
-// TODO: abstract the CRUD methods below
-
-function output_template() {
-	template = '' +
-		'{{#errors}}' +
-		'<b style="color: red;"> ERROR:</b> {{.}} <br>\n' +
-		'{{/errors}}' +
-		'{{#result}}' +
-		'{{.}}<br>\n' +
-		'{{/result}}';
-	return template
-}
-
-function menu_template(theoptions) {
-	template = '' +
-		'<option></option>\n' +
-		'{{#' + theoptions + '}}' +
-		'<option value="{{id}}" description="{{description}}">{{name}}</option>\n' +
-		'{{/' + theoptions + '}}';
-	return template
-}
-
-function clear_field(thefield) {
-	document.getElementById(thefield).value = 0;
-}
-
-async function _update_data_via_api(theurl, themethod, theform) {
-	let target = document.getElementById("outputBox");
-	let response = await fetch(
-		theurl, {
-		method: themethod,
-		body: new FormData(theform)
-	});
-	let json = await response.json();
-	target.innerHTML = Mustache.render(output_template(), json);
-	return response.status;
-}
-
-async function load_setup_errors() {
-	let target = document.getElementById("outputBox");
-	let template = '' +
-		'{{#setup_errors}}' +
-		'<b style="color: red;">ERROR:</b> {{.}}' +
-		'{{/setup_errors}}';
-	let response = await fetch("/summary/");
-	let json = await response.json();
-	target.innerHTML = Mustache.render(template, json);
-}
+// Data getters
 
 async function load_atmospheric_conditions() {
 	let target = document.getElementById("atmosphericConditions");
@@ -53,32 +6,6 @@ async function load_atmospheric_conditions() {
 	let response = await fetch("/atmosphere/");
 	let json = await response.json();
 	target.innerHTML = Mustache.render(template, json);
-}
-
-async function load_prism_offsets() {
-	let target = document.getElementById("prismOffsets");
-	let template = "Prism Offsets: {{.}}";
-	let response = await fetch("/prism/");
-	let json = await response.json();
-	target.innerHTML = Mustache.render(template, json);
-}
-
-async function set_atmospheric_conditions() {
-	let result = await _update_data_via_api("/atmosphere/", "PUT", setAtmosphericConditionsForm);
-	load_atmospheric_conditions();
-}
-
-async function set_prism_offsets() {
-	let result = await _update_data_via_api("/prism/", "PUT", setPrismOffsetsForm);
-	load_prism_offsets();
-}
-
-async function load_sites_menus() {
-	let response = await fetch("/site/");
-	let json = await response.json();
-	Array.from(document.querySelectorAll('.sitesmenu')).forEach(function (target) {
-		target.innerHTML = Mustache.render(menu_template('sites'), json);
-	});
 }
 
 async function load_classes_menus() {
@@ -97,18 +24,53 @@ async function load_geometries_menus() {
 	});
 }
 
-async function set_configs() {
-	let result = await _update_data_via_api("/config/", "PUT", setConfigsForm);
-	if (result >= 200 && result <= 299) {
-		document.getElementById("setConfigsForm").reset();
-	}
+async function load_prism_offsets() {
+	let target = document.getElementById("prismOffsets");
+	let template = "Prism Offsets: {{.}}";
+	let response = await fetch("/prism/");
+	let json = await response.json();
+	target.innerHTML = Mustache.render(template, json);
 }
 
-async function save_new_site() {
-	let result = await _update_data_via_api("/site/", "POST", saveNewSiteForm);
+async function load_setup_errors() {
+	let target = document.getElementById("outputBox");
+	let template = '' +
+		'{{#setup_errors}}' +
+		'<b style="color: red;">ERROR:</b> {{.}}' +
+		'{{/setup_errors}}';
+	let response = await fetch("/summary/");
+	let json = await response.json();
+	target.innerHTML = Mustache.render(template, json);
+}
+
+async function load_sites_menus() {
+	let response = await fetch("/site/");
+	let json = await response.json();
+	Array.from(document.querySelectorAll('.sitesmenu')).forEach(function (target) {
+		target.innerHTML = Mustache.render(menu_template('sites'), json);
+	});
+}
+
+
+// Data setters
+
+async function _update_data_via_api(theurl, themethod, theform) {
+	let target = document.getElementById("outputBox");
+	let response = await fetch(
+		theurl, {
+		method: themethod,
+		body: new FormData(theform)
+	});
+	let json = await response.json();
+	target.innerHTML = Mustache.render(output_template(), json);
+	return response.status;
+}
+
+async function delete_class() {
+	let result = await _update_data_via_api("/class/", "DELETE", deleteClassForm)
 	if (result >= 200 && result <= 299) {
-		document.getElementById("saveNewSiteForm").reset();
-		load_sites_menus();
+		document.getElementById("deleteClassForm").reset();
+		load_classes_menus();
 	}
 }
 
@@ -120,41 +82,11 @@ async function delete_site() {
 	}
 }
 
-async function save_new_station() {
-	let result = await _update_data_via_api("/station/", "POST", saveNewStationForm);
-	if (result >= 200 && result <= 299) {
-		document.getElementById("saveNewStationForm").reset();
-	}
-}
-
 async function delete_station() {
 	let result = await _update_data_via_api("/station/", "DELETE", deleteStationForm)
 	if (result >= 200 && result <= 299) {
 		document.getElementById("deleteStationForm").reset();
 		document.getElementById("deleteStationFormStationsMenu").innerHTML = "";
-	}
-}
-
-async function save_new_class() {
-	let result = await _update_data_via_api("/class/", "POST", saveNewClassForm);
-	if (result >= 200 && result <= 299) {
-		document.getElementById("saveNewClassForm").reset();
-		load_classes_menus();
-	}
-}
-
-async function delete_class() {
-	let result = await _update_data_via_api("/class/", "DELETE", deleteClassForm)
-	if (result >= 200 && result <= 299) {
-		document.getElementById("deleteClassForm").reset();
-		load_classes_menus();
-	}
-}
-
-async function save_new_subclass() {
-	let result = await _update_data_via_api("/subclass/", "POST", saveNewSubclassForm);
-	if (result >= 200 && result <= 299) {
-		document.getElementById("saveNewSubclassForm").reset();
 	}
 }
 
@@ -166,43 +98,89 @@ async function delete_subclass() {
 	}
 }
 
-async function update_dependent_station_menu(thesite, themenu) {
-	let target = document.getElementById(themenu);
-	if (thesite.value === "") {
-		target.innerHTML = "";
-	} else {
-		let response = await fetch("/station/" + thesite.value);
-		let json = await response.json();
-		target.innerHTML = Mustache.render(menu_template('stations'), json);
+async function save_new_class() {
+	let result = await _update_data_via_api("/class/", "POST", saveNewClassForm);
+	if (result >= 200 && result <= 299) {
+		document.getElementById("saveNewClassForm").reset();
+		load_classes_menus();
 	}
 }
 
-async function update_dependent_subclass_menu(theclass, themenu) {
-	let target = document.getElementById(themenu);
-	if (theclass.value === "") {
-		target.innerHTML = "";
-	} else {
-		let response = await fetch("/subclass/" + theclass.value);
-		let json = await response.json();
-		target.innerHTML = Mustache.render(menu_template('subclasses'), json);
+async function save_new_site() {
+	let result = await _update_data_via_api("/site/", "POST", saveNewSiteForm);
+	if (result >= 200 && result <= 299) {
+		document.getElementById("saveNewSiteForm").reset();
+		load_sites_menus();
 	}
 }
 
-function update_backsight_station_menu(occupiedstationmenu) {
-	let options = occupiedstationmenu.innerHTML.split("\n");
-	let newoptions = Array();
-	options.forEach((option) => {
-		if (option.indexOf('value="' + occupiedstationmenu.value + '"') === -1) {
-			newoptions.push(option);
-		}
-	});
-	document.getElementById("startNewSessionFormBacksightStationMenu").innerHTML = newoptions.join("\n");
+async function save_new_station() {
+	let result = await _update_data_via_api("/station/", "POST", saveNewStationForm);
+	if (result >= 200 && result <= 299) {
+		document.getElementById("saveNewStationForm").reset();
+	}
+}
+
+async function save_new_subclass() {
+	let result = await _update_data_via_api("/subclass/", "POST", saveNewSubclassForm);
+	if (result >= 200 && result <= 299) {
+		document.getElementById("saveNewSubclassForm").reset();
+	}
+}
+
+async function set_atmospheric_conditions() {
+	let result = await _update_data_via_api("/atmosphere/", "PUT", setAtmosphericConditionsForm);
+	load_atmospheric_conditions();
+}
+
+async function set_configs() {
+	let result = await _update_data_via_api("/config/", "PUT", setConfigsForm);
+	if (result >= 200 && result <= 299) {
+		document.getElementById("setConfigsForm").reset();
+	}
+}
+
+async function set_prism_offsets() {
+	let result = await _update_data_via_api("/prism/", "PUT", setPrismOffsetsForm);
+	load_prism_offsets();
 }
 
 
+// UI manipulators
 
-// Custom helper functions to provide functionality that isn't directly available in htmx.
+function _hide_required_field(thefield) {
+	document.getElementById(thefield + "Label").hidden = true;
+	document.getElementById(thefield).hidden = true;
+	try {
+		document.getElementById(thefield.replace("Menu", "Description")).hidden = true;
+	} catch {
+		// ignore the error
+	}
+	document.getElementById(thefield).required = false;
+}
 
+function _show_required_field(thefield) {
+	document.getElementById(thefield + "Label").hidden = false;
+	document.getElementById(thefield).hidden = false;
+	try {
+		document.getElementById(thefield.replace("Menu", "Description")).hidden = false;
+	} catch {
+		// ignore the error
+	}
+	document.getElementById(thefield).required = true;
+}
+
+function clear_field(thefield) {
+	document.getElementById(thefield).value = 0;
+}
+
+function collapse_section(thesection) {
+	thesection.parentNode.querySelector(".sectionforms").hidden = !thesection.parentNode.querySelector(".sectionforms").hidden;
+	let collapser = thesection.parentNode.querySelector(".collapser");
+	collapser.hidden = !collapser.hidden;
+	let expander = thesection.parentNode.querySelector(".expander");
+	expander.hidden = !expander.hidden;
+}
 
 function show_on_the_fly_adjustments_popup() {
 	fetch("/atmosphere/")
@@ -243,109 +221,6 @@ function show_on_the_fly_adjustments_popup() {
 	thepopup.hidden = false;
 }
 
-function collapse_section(thesection) {
-	thesection.parentNode.querySelector(".sectionforms").hidden = !thesection.parentNode.querySelector(".sectionforms").hidden;
-	let collapser = thesection.parentNode.querySelector(".collapser");
-	collapser.hidden = !collapser.hidden;
-	let expander = thesection.parentNode.querySelector(".expander");
-	expander.hidden = !expander.hidden;
-}
-
-function show_required_field(thefield) {
-	document.getElementById(thefield + "Label").hidden = false;
-	document.getElementById(thefield).hidden = false;
-	try {
-		document.getElementById(thefield.replace("Menu", "Description")).hidden = false;
-	} catch {
-		// ignore the error
-	}
-	document.getElementById(thefield).required = true;
-}
-
-function hide_required_field(thefield) {
-	document.getElementById(thefield + "Label").hidden = true;
-	document.getElementById(thefield).hidden = true;
-	try {
-		document.getElementById(thefield.replace("Menu", "Description")).hidden = true;
-	} catch {
-		// ignore the error
-	}
-	document.getElementById(thefield).required = false;
-}
-
-function update_description(source, target) {
-	let thedescription = source.options[source.selectedIndex].getAttribute("description");
-	if (thedescription === "") {
-		thedescription = "(no description recorded)"
-	}
-	if (thedescription === null) {
-		document.getElementById(target).removeAttribute("onclick");
-	} else {
-		thedescription = thedescription.replaceAll("\\", "\\\\");
-		thedescription = thedescription.replaceAll("\'", "\\\'");
-		thedescription = thedescription.replaceAll("\"", "\\\"");
-		document.getElementById(target).setAttribute("onclick", "alert('" + thedescription + "')");
-	}
-}
-
-function update_required_new_site_fields(coordinatesystemmenu) {
-	switch (coordinatesystemmenu.value) {
-		case "Site":
-			show_required_field("saveNewStationNorthing");
-			show_required_field("saveNewStationEasting");
-			show_required_field("saveNewStationElevation");
-			hide_required_field("saveNewStationUTMZone");
-			hide_required_field("saveNewStationLatitude");
-			hide_required_field("saveNewStationLongitude");
-			break;
-		case "UTM":
-			show_required_field("saveNewStationNorthing");
-			show_required_field("saveNewStationEasting");
-			show_required_field("saveNewStationElevation");
-			show_required_field("saveNewStationUTMZone");
-			hide_required_field("saveNewStationLatitude");
-			hide_required_field("saveNewStationLongitude");
-			break;
-		case "Lat/Lon":
-			hide_required_field("saveNewStationNorthing");
-			hide_required_field("saveNewStationEasting");
-			hide_required_field("saveNewStationUTMZone");
-			show_required_field("saveNewStationLatitude");
-			show_required_field("saveNewStationLongitude");
-			show_required_field("saveNewStationElevation");
-			break;
-		default:
-			hide_required_field("saveNewStationNorthing");
-			hide_required_field("saveNewStationEasting");
-			hide_required_field("saveNewStationElevation");
-			hide_required_field("saveNewStationUTMZone");
-			hide_required_field("saveNewStationLatitude");
-			hide_required_field("saveNewStationLongitude");
-	}
-}
-
-function update_required_new_session_fields(sessiontypemenu) {
-	switch (sessiontypemenu.value) {
-		case "Backsight":
-			show_required_field("startNewSessionFormBacksightStationMenu");
-			show_required_field("startNewSessionFormPrismHeight");
-			hide_required_field("startNewSessionFormInstrumentHeight");
-			hide_required_field("startNewSessionFormAzimuth");
-			break;
-		case "Azimuth":
-			hide_required_field("startNewSessionFormBacksightStationMenu");
-			hide_required_field("startNewSessionFormPrismHeight");
-			show_required_field("startNewSessionFormInstrumentHeight");
-			show_required_field("startNewSessionFormAzimuth");
-			break;
-		default:
-			hide_required_field("startNewSessionFormBacksightStationMenu");
-			hide_required_field("startNewSessionFormPrismHeight");
-			hide_required_field("startNewSessionFormInstrumentHeight");
-			hide_required_field("startNewSessionFormAzimuth");
-	}
-}
-
 function show_shot_form(theform) {
 	switch (theform) {
 		case "takeShotForm":
@@ -364,4 +239,133 @@ function show_shot_form(theform) {
 			document.getElementById("takeShotForm").hidden = false;
 			break;
 	}
+}
+
+function update_backsight_station_menu(occupiedstationmenu) {
+	let options = occupiedstationmenu.innerHTML.split("\n");
+	let newoptions = Array();
+	options.forEach((option) => {
+		if (option.indexOf('value="' + occupiedstationmenu.value + '"') === -1) {
+			newoptions.push(option);
+		}
+	});
+	document.getElementById("startNewSessionFormBacksightStationMenu").innerHTML = newoptions.join("\n");
+}
+
+async function update_dependent_station_menu(thesite, themenu) {
+	let target = document.getElementById(themenu);
+	if (thesite.value === "") {
+		target.innerHTML = "";
+	} else {
+		let response = await fetch("/station/" + thesite.value);
+		let json = await response.json();
+		target.innerHTML = Mustache.render(menu_template('stations'), json);
+	}
+}
+
+async function update_dependent_subclass_menu(theclass, themenu) {
+	let target = document.getElementById(themenu);
+	if (theclass.value === "") {
+		target.innerHTML = "";
+	} else {
+		let response = await fetch("/subclass/" + theclass.value);
+		let json = await response.json();
+		target.innerHTML = Mustache.render(menu_template('subclasses'), json);
+	}
+}
+
+function update_description(source, target) {
+	let thedescription = source.options[source.selectedIndex].getAttribute("description");
+	if (thedescription === "") {
+		thedescription = "(no description recorded)"
+	}
+	if (thedescription === null) {
+		document.getElementById(target).removeAttribute("onclick");
+	} else {
+		thedescription = thedescription.replaceAll("\\", "\\\\");
+		thedescription = thedescription.replaceAll("\'", "\\\'");
+		thedescription = thedescription.replaceAll("\"", "\\\"");
+		document.getElementById(target).setAttribute("onclick", "alert('" + thedescription + "')");
+	}
+}
+
+function update_required_new_session_fields(sessiontypemenu) {
+	switch (sessiontypemenu.value) {
+		case "Backsight":
+			_show_required_field("startNewSessionFormBacksightStationMenu");
+			_show_required_field("startNewSessionFormPrismHeight");
+			_hide_required_field("startNewSessionFormInstrumentHeight");
+			_hide_required_field("startNewSessionFormAzimuth");
+			break;
+		case "Azimuth":
+			_hide_required_field("startNewSessionFormBacksightStationMenu");
+			_hide_required_field("startNewSessionFormPrismHeight");
+			_show_required_field("startNewSessionFormInstrumentHeight");
+			_show_required_field("startNewSessionFormAzimuth");
+			break;
+		default:
+			_hide_required_field("startNewSessionFormBacksightStationMenu");
+			_hide_required_field("startNewSessionFormPrismHeight");
+			_hide_required_field("startNewSessionFormInstrumentHeight");
+			_hide_required_field("startNewSessionFormAzimuth");
+	}
+}
+
+function update_required_new_site_fields(coordinatesystemmenu) {
+	switch (coordinatesystemmenu.value) {
+		case "Site":
+			_show_required_field("saveNewStationNorthing");
+			_show_required_field("saveNewStationEasting");
+			_show_required_field("saveNewStationElevation");
+			_hide_required_field("saveNewStationUTMZone");
+			_hide_required_field("saveNewStationLatitude");
+			_hide_required_field("saveNewStationLongitude");
+			break;
+		case "UTM":
+			_show_required_field("saveNewStationNorthing");
+			_show_required_field("saveNewStationEasting");
+			_show_required_field("saveNewStationElevation");
+			_show_required_field("saveNewStationUTMZone");
+			_hide_required_field("saveNewStationLatitude");
+			_hide_required_field("saveNewStationLongitude");
+			break;
+		case "Lat/Lon":
+			_hide_required_field("saveNewStationNorthing");
+			_hide_required_field("saveNewStationEasting");
+			_hide_required_field("saveNewStationUTMZone");
+			_show_required_field("saveNewStationLatitude");
+			_show_required_field("saveNewStationLongitude");
+			_show_required_field("saveNewStationElevation");
+			break;
+		default:
+			_hide_required_field("saveNewStationNorthing");
+			_hide_required_field("saveNewStationEasting");
+			_hide_required_field("saveNewStationElevation");
+			_hide_required_field("saveNewStationUTMZone");
+			_hide_required_field("saveNewStationLatitude");
+			_hide_required_field("saveNewStationLongitude");
+	}
+}
+
+
+// Mustache templates
+
+function menu_template(theoptions) {
+	template = '' +
+		'<option></option>\n' +
+		'{{#' + theoptions + '}}' +
+		'<option value="{{id}}" description="{{description}}">{{name}}</option>\n' +
+		'{{/' + theoptions + '}}';
+	return template
+}
+
+function output_template() {
+	template = '' +
+		'{{#errors}}' +
+		'<b style="color: red;"> ERROR:</b> {{.}} <br>\n' +
+		'{{/errors}}' +
+		'{{#result}}' +
+		'{{.}}<br>\n' +
+		'{{/result}}';
+	return template
 }
